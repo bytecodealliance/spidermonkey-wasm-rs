a45877f2270c400c918104cf68eeac308dd8b182
Saúl Cabrera <saulecabrera@gmail.com>
2022-01-12 16:27:06 -0500

    feat: Add `jsffi` mod

--- crates/spidermonkey-wasm-sys/src/lib.rs
+++ crates/spidermonkey-wasm-sys/src/lib.rs
@@ -1,5 +1,53 @@
-pub use js_bindings::root as api;
+extern crate link_cplusplus;
 
-pub mod js_bindings {
-    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+mod jsgc;
+
+#[cxx::bridge]
+pub mod jsffi {
+    #[repr(u32)]
+    #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+    #[namespace = "JS"]
+    enum OnNewGlobalHookOption {
+        FireOnNewGlobalHook = 0,
+        DontFireOnNewGlobalHook = 1,
+    }
+
+    unsafe extern "C++" {
+        include!("api.h");
+
+        type JSObject;
+        type JSRuntime;
+        type JSContext;
+        type JSClass;
+        type JSPrincipals;
+        type RootingContext = crate::jsgc::RootingContext;
+        type RootedObject = crate::jsgc::Rooted<*const JSObject>;
+        #[namespace = "JS"]
+        type RootKind = crate::jsgc::RootKind;
+        #[namespace = "JS"]
+        type AutoRooterListHeads;
+        #[namespace = "js"]
+        type GeckoProfilerThread;
+        #[namespace = "JS"]
+        type Realm;
+        #[namespace = "JS"]
+        type Zone;
+        #[namespace = "JS"]
+        type RealmOptions;
+        #[namespace = "JS"]
+        type OnNewGlobalHookOption; 
+
+        fn JS_Init() -> bool;
+        fn getDefaultGlobalClass() -> UniquePtr<JSClass>;
+        fn makeDefaultRealmOptions() -> *mut RealmOptions;
+
+        unsafe fn JS_NewContext(max_bytes: u32, parent: *mut JSRuntime) -> *mut JSContext;
+        unsafe fn JS_NewGlobalObject(
+            context: *mut JSContext,
+            klass: *const JSClass,
+            principals: *mut JSPrincipals,
+            hook: OnNewGlobalHookOption,
+            realm_opts: &RealmOptions
+        ) -> *mut JSObject;
+    }
 }
