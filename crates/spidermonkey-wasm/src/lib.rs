88a1a790d6ade633060304bbcff4b7de145f3538
Saúl Cabrera <saulecabrera@gmail.com>
2022-02-10 17:06:21 -0500

    feat: Temporarily expose jsffi through jsapi

--- crates/spidermonkey-wasm/src/lib.rs
+++ crates/spidermonkey-wasm/src/lib.rs
@@ -1,3 +1,9 @@
 pub mod runtime;
 pub mod rooted;
 pub mod handle;
+
+pub mod jsapi {
+    pub use spidermonkey_wasm_sys::jsffi::*;
+    pub use spidermonkey_wasm_sys::jsrealm; 
+    pub use spidermonkey_wasm_sys::jsgc; 
+}
