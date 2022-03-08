a8bec8e66f152d7c586478992ae8d61810bc5238
Saúl Cabrera <saulecabrera@gmail.com>
2022-03-08 14:35:56 -0500

    ci: Check repl example

--- crates/repl-example/build.sh
+++ crates/repl-example/build.sh
@@ -8,4 +8,8 @@ export AR="/opt/wasi-sdk/wasi-sdk-12.0/bin/ar"
 export LIBCLANG_PATH="/opt/wasi-sdk/wasi-sdk-12.0/share/wasi-sysroot/lib/wasm32-wasi"
 export LIBCLANG_RT_PATH="/opt/wasi-sdk/wasi-sdk-12.0/lib/clang/11.0.0/lib/wasi"
 
-cargo $@
+if [ $# -eq 0 ]; then
+  cargo build
+else
+  cargo "$@"
+fi
